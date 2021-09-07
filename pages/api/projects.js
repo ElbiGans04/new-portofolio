03b80db881c576a501bd61191689ee72ecf8f6d9
rhafael bijaksana <rhafaelbijaksana04@gmail.com>
2021-09-07 22:17:13 +0700

    menambahkan multer ke project api

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -1,9 +1,32 @@
 import dbConnect from "../../database/connection";
 import Project from "../../database/schemas/projects";
 import TypeProject from "../../database/schemas/typeProject";
-// import Multer from 'multer'
+import Multer from 'multer'
+import path from 'path'
 
-// const multer = Multer({dest: '/temp'}).any()
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+
+const storage = Multer.diskStorage({
+  destination: function (req, file, cb) {
+    const currentDirectory = path.resolve(process.cwd(), 'public/images/tmp');
+    cb(null, currentDirectory);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, `elbi-project-${uniqueSuffix}-${file.fieldname}.jpg`);
+  }
+});
+
+const multer = Multer({storage: storage, limits: {
+  fileSize: 400000,
+  files: 5,
+}})
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
@@ -11,22 +34,33 @@ function runMiddleware(req, res, fn) {
   return new Promise((resolve, reject) => {
     fn(req, res, (result) => {
       if (result instanceof Error) {
-        return reject(result)
+        return reject(result);
       }
 
-      return resolve(result)
-    })
-  })
+      return resolve(result);
+    });
+  });
 }
-
 
 export default async function handler(req, res) {
   const { method } = req;
-  await dbConnect();
+  req.body = !req.body ? {} : req.body;
+  console.log(req.body)
+  // const {
+  //   title = "",
+  //   startDate = Date.now(),
+  //   endDate = Date.now(),
+  //   tools = [],
+  //   typeProject = 'A1',
+  //   images = [],
+  //   description = '',
+  //   url = ''
+  // } = req.body;
+  try {
+    await dbConnect();
 
-  switch (method) {
-    case "GET":
-      try {
+    switch (method) {
+      case "GET":
         const { type = "ALL", order = "ASC" } = req.query;
         let results;
 
@@ -38,32 +72,27 @@ export default async function handler(req, res) {
           results = (
             await TypeProject.findOne({ _id: type })
               .sort({ title: order === "ASC" ? 1 : -1 })
-              .populate({ path: "projects", populate: 'typeProject' })
+              .populate({ path: "projects", populate: "typeProject" })
           ).projects;
         }
 
+        res.status(200).json({ data: results });
 
-        res.status(200).json({ success: true, data: results });
-      
-      } catch (error) {
-        console.log(error);
-        res.status(400).json({ success: false });
-      }
-      break;
-    case "POST":
-      try {
-        // await runMiddleware(req, res, multer)
-        console.log(req.body);
-        console.log(req.file)
-        res.status(201).json({ success: true,});
-      } catch (error) {
-        console.log(error);
-        res.status(400).json({ success: false });
-      }
-      break;
-    default:
-      res.status(405).json({ errors: { code: 405, title: 'method not found' } });
-      break;
+        break;
+      case "POST":
+        await runMiddleware(req, res, multer.array('images', 5));
+        console.log(req.files)
+        res.send("OK")
+        break;
+      default:
+        res
+          .status(405)
+          .json({ errors: { code: 405, title: "method not found" } });
+        break;
+    }
+  } catch (err) {
+    console.log(err)
+    res.status(400).json({ success: false });
   }
   // res.json({vv:'s'})
 }
