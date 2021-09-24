5a427c03f9255a3da0f1d8e45c73fefa1c71c918
rhafael bijaksana <rhafaelbijaksana04@gmail.com>
2021-09-24 17:23:36 +0700

    membuat agar image blur saat meload dihalaman index

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,37 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import React from 'react'
-import styleIndex from '../styles/index.module.css'
-import styled from 'styled-components'
-import Heading from '../Components/Heading'
+import Head from "next/head";
+import Image from "next/image";
+import React from "react";
+import styleIndex from "../styles/index.module.css";
+import styled from "styled-components";
+import Heading from "../Components/Heading";
+import ProfileImage from "../public/images/profile.jpg";
 
 export default function Home() {
   return (
-      <Container>
-        <Head>
-          <title>Welcome</title>
-        </Head>
-        <ContainerImage>
-          <Image className={styleIndex.profile} layout="fill" src="/images/profile.jpg"></Image>
-        </ContainerImage>
-        <TextParent> 
-          <Heading size={2.5} minSize={1.5}>Hello, i'm <span>Rhafael Bijaksana</span></Heading>
-          <Heading size={2} minSize={1.3}>I'm a <span>Fullstack Developer</span></Heading>
-          {/* <Heading>Reach me via email <span>rhafaelbijaksana04@gmail.com</span></Heading> */}
-        </TextParent>
-      </Container>
-  )
+    <Container>
+      <Head>
+        <title>Welcome</title>
+      </Head>
+      <ContainerImage>
+        <Image
+          className={styleIndex.profile}
+          layout="fill"
+          placeholder="blur"
+          src={ProfileImage}
+        ></Image>
+      </ContainerImage>
+      <TextParent>
+        <Heading size={2.5} minSize={1.5}>
+          Hello, i'm <span>Rhafael Bijaksana</span>
+        </Heading>
+        <Heading size={2} minSize={1.3}>
+          I'm a <span>Fullstack Developer</span>
+        </Heading>
+        {/* <Heading>Reach me via email <span>rhafaelbijaksana04@gmail.com</span></Heading> */}
+      </TextParent>
+    </Container>
+  );
 }
-
 
 const Container = styled.div`
   width: 100%;
@@ -41,11 +50,13 @@ const ContainerImage = styled.div`
   width: 300px;
   height: 300px;
   position: relative;
+  border: 0.3rem solid var(--pink) !important;
+  border-radius: 50%;
 
   @media (max-width: 576px) {
     & {
       width: 250px;
-      height: 250px
+      height: 250px;
     }
   }
 `;
