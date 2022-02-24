92da2cf61b9e0d0ff6c3ef8c7bd78e20df2440be
rhafael <rhafaelbijaksana04@gmail.com>
2022-02-24 19:41:22 +0700

    feat: membuat file src/database/index.ts sebagai entry point schemas mongoose

---
+++ src/database/index.ts
@@ -0,0 +1,3 @@
+export { default as typeProjectSchema } from './schemas/typeProject';
+export { default as toolSchema } from './schemas/tools';
+export { default as projectsSchema } from './schemas/projects';
