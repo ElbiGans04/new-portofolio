64c658474741144ee780f042b1ce46cfb681d5a0
rhafael <rhafaelbijaksana04@gmail.com>
2022-03-08 12:11:20 +0700

    refactor: mengubah tulisan pada halaman tools

--- src/pages/tools.tsx
+++ src/pages/tools.tsx
@@ -11,7 +11,7 @@ function About() {
       <Paragraph minSize={1} size={1.5} lineHeight="2">
         When I develop a web application I usually use{' '}
         <span>react or next js</span> as the frontend framework,{' '}
-        <span>Redux</span> for state management, use{' '}
+        <span>typescript</span> to make the code more predictable, use{' '}
         <span>mysql or mongodb</span> as the database,
         <span>express</span> js for the backend framework
       </Paragraph>
