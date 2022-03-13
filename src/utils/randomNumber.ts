1d60b6a51a31eb8b97178537fb81d6630c135fe4
rhafael <rhafaelbijaksana04@gmail.com>
2022-03-13 19:21:04 +0700

    fix: mengatasi error ditemukan key yang sama

--- src/utils/randomNumber.ts
+++ src/utils/randomNumber.ts
@@ -1,3 +1,7 @@
 export default function getRandom(index: number) {
-  return `${Date.now()}${Math.floor(Math.random() * (1 - 100) + 100)}${index}`;
+  return `${Date.now()}${Math.floor(
+    Math.random() * (1 - 100) + 100,
+  )}${index}${Math.floor(Math.random() * (1 - 100) + 100)}${index}${Math.floor(
+    Math.random() * (1 - 100) + 100,
+  )}`;
 }
