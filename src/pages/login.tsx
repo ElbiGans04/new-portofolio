923c0a92e90424c0a2518eba4b0405b49ea494d1
rhafael-bijaksana <rhafaelbijaksana04@gmail.com>
2022-04-02 16:05:20 +0700

    feat!: mengubah state pada halaman login
    menambahkan loading pada halaman login dan
    mengubah state pada halaman tersebut

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -23,59 +23,80 @@ export default function Login() {
     password: string;
   }>();
   const ref = useRef<HTMLDivElement>(null);
-  const [errorMessage, setErrorMessage] = useState<null | string>(null);
+  const [state, setState] = useState<
+    { status: 'iddle'; message?: string } | { status: 'loading' }
+  >({ status: 'iddle' });
   const router = useRouter();
 
   const onSubmit = (e: React.BaseSyntheticEvent) => {
-    e.preventDefault();
-    const callback = handleSubmit((data) => {
-      fetcherGeneric<DocMeta>('/api/auth/login', {
-        method: 'post',
-        body: JSON.stringify({ type: 'Admin', attributes: data }),
-        headers: {
-          'Content-Type': 'application/vnd.api+json',
-        },
-      })
-        .then(() => {
-          router.push('/admin/projects').catch((err) => console.log(err));
+    if (state.status === 'iddle') {
+      e.preventDefault();
+      const callback = handleSubmit((data) => {
+        setState({ status: 'loading' });
+        fetcherGeneric<DocMeta>('/api/auth/login', {
+          method: 'post',
+          body: JSON.stringify({ type: 'Admin', attributes: data }),
+          headers: {
+            'Content-Type': 'application/vnd.api+json',
+          },
         })
-        .catch((err) => {
-          const message =
-            err instanceof HttpError ? err.message : 'Error when request';
-          setErrorMessage(message);
-        });
-    });
+          .then((res) => {
+            console.log(res);
+            router.push('/admin/projects').catch((err) => console.log(err));
+          })
+          .catch((err) => {
+            const message =
+              err instanceof HttpError ? err.message : 'Error when request';
+            setState({ status: 'iddle', message });
+          });
+      });
 
-    callback().catch((err) => console.error(err));
+      callback().catch((err) => console.error(err));
+    }
   };
 
   return (
-    <Container>
+    <Container
+      boxShadow={
+        state.status === 'loading'
+          ? undefined
+          : '2px 2px 7px rgba(0, 0, 0, 0.5)'
+      }
+      backgroundColor={state.status === 'loading' ? 'var(--dark)' : undefined}
+    >
       <Head>
         <title>Login</title>
       </Head>
 
       {/* Transition */}
       <Transition
-        in={errorMessage === null ? false : true}
+        in={state.status === 'iddle' && state.message !== undefined}
         timeout={500}
         nodeRef={ref}
       >
         {() => (
           <BoxCollapse
             maxHeight={
-              ref.current && errorMessage !== null
+              ref.current &&
+              state.status === 'iddle' &&
+              state.message !== undefined
                 ? ref.current.scrollHeight
                 : 0
             }
             ref={ref}
           >
             <MessageParent>
-              <p>{errorMessage && upperFirstWord(errorMessage)}</p>
+              <p>
+                {state.status === 'iddle' &&
+                  state.message !== undefined &&
+                  upperFirstWord(state.message)}
+              </p>
               <AiOutlineClose
                 title="close message"
                 style={{ cursor: 'pointer' }}
-                onClick={() => setErrorMessage(null)}
+                onClick={() =>
+                  setState({ status: 'iddle', message: undefined })
+                }
                 size="1.3em"
               />
             </MessageParent>
@@ -83,65 +104,73 @@ export default function Login() {
         )}
       </Transition>
       <Form onSubmit={onSubmit}>
-        <FormRow>
-          <Label size={1} minSize={1} htmlFor="email">
-            Email :
-          </Label>
-          <Input
-            placeholder="Enter your email"
-            type="email"
-            id="email"
-            borderColor={errors.email && 'var(--red2)'}
-            borderColor2={errors.email && 'var(--red)'}
-            {...register('email', {
-              required: { value: true, message: 'Please insert email' },
-              pattern: {
-                value:
-                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                message: 'Please insert valid email',
-              },
-            })}
-          />
-          {errors.email?.message && (
-            <p
-              style={{
-                fontSize: '.9rem',
-                color: 'var(--red)',
-                margin: '0.5rem 0',
-              }}
-            >
-              {errors.email.message}
-            </p>
-          )}
-        </FormRow>
-        <FormRow>
-          <Label size={1} minSize={1} htmlFor="password">
-            Password :
-          </Label>
-          <Input
-            placeholder="Enter your password"
-            type="password"
-            id="password"
-            borderColor={errors.password && 'var(--red2)'}
-            borderColor2={errors.password && 'var(--red)'}
-            {...register('password', {
-              required: { value: true, message: 'Pleas insert your password' },
-            })}
-          />
-          {errors.password?.message && (
-            <p
-              style={{
-                fontSize: '.9rem',
-                color: 'var(--red)',
-                margin: '0.5rem 0',
-              }}
-            >
-              {errors.password.message}
-            </p>
-          )}
-        </FormRow>
-
-        <NewButton type="submit">Login</NewButton>
+        {state.status == 'loading' ? (
+          <div className="loader"></div>
+        ) : (
+          <React.Fragment>
+            <FormRow>
+              <Label size={1} minSize={1} htmlFor="email">
+                Email :
+              </Label>
+              <Input
+                placeholder="Enter your email"
+                type="email"
+                id="email"
+                borderColor={errors.email && 'var(--red2)'}
+                borderColor2={errors.email && 'var(--red)'}
+                {...register('email', {
+                  required: { value: true, message: 'Please insert email' },
+                  pattern: {
+                    value:
+                      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                    message: 'Please insert valid email',
+                  },
+                })}
+              />
+              {errors.email?.message && (
+                <p
+                  style={{
+                    fontSize: '.9rem',
+                    color: 'var(--red)',
+                    margin: '0.5rem 0',
+                  }}
+                >
+                  {errors.email.message}
+                </p>
+              )}
+            </FormRow>
+            <FormRow>
+              <Label size={1} minSize={1} htmlFor="password">
+                Password :
+              </Label>
+              <Input
+                placeholder="Enter your password"
+                type="password"
+                id="password"
+                borderColor={errors.password && 'var(--red2)'}
+                borderColor2={errors.password && 'var(--red)'}
+                {...register('password', {
+                  required: {
+                    value: true,
+                    message: 'Pleas insert your password',
+                  },
+                })}
+              />
+              {errors.password?.message && (
+                <p
+                  style={{
+                    fontSize: '.9rem',
+                    color: 'var(--red)',
+                    margin: '0.5rem 0',
+                  }}
+                >
+                  {errors.password.message}
+                </p>
+              )}
+            </FormRow>
+            <NewButton type="submit">Login</NewButton>
+          </React.Fragment>
+        )}
       </Form>
     </Container>
   );
@@ -167,12 +196,13 @@ const MessageParent = styled.div`
   height: 100%;
 `;
 
-const Container = styled.div`
+const Container = styled.div<{ backgroundColor?: string; boxShadow?: string }>`
   width: 50%;
-  background-color: var(--dark2);
+  background-color: ${({ backgroundColor }) =>
+    backgroundColor || 'var(--dark2)'};
   padding: 2rem;
   border-radius: 1rem;
-  box-shadow: 2px 2px 7px rgba(0, 0, 0, 0.5);
+  box-shadow: ${({ boxShadow }) => boxShadow || ''};
 
   @media (max-width: 768px) {
     & {
